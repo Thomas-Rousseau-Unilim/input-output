a6e9d996121ad00486a8fb7786bf7a785005caa4
rousseau129 <thomas.rousseau2@etu.unilim.fr>
2026-04-27 16:09:47 +0200

    delete showchocolatemenu from displaycontrol

--- src/main/java/io/gui/console/DisplayConsole.java
+++ src/main/java/io/gui/console/DisplayConsole.java
@@ -11,15 +11,6 @@ public class DisplayConsole extends Console {
 	private static final String EXIT_KEY = "0";
 	private static final String EXIT_TEXT = "Exit";
 	private static final String EXIT_OPTION_LINE = EXIT_KEY + "." + EXIT_TEXT;
-
-	public String showChocolateMenu() {
-		String menu = "";
-		menu = menu + "1.Dark Chocolate" + LINE_BREAK;
-		menu = menu + "2.Milk Chocolate" + LINE_BREAK;
-		menu = menu + "3.White Chocolate" + LINE_BREAK;
-		menu = menu + EXIT_OPTION_LINE + LINE_BREAK;
-		return menu;
-	}
 	
 	public String menu(String... options) {
 
